c56b6d782270dd18fc0b9839c3f87ff3d80f43cc
annitaannie <159273004+annitaannie@users.noreply.github.com>
2024-02-29 18:38:46 +0300

    break.py

--- break.py
+++ break.py
@@ -1,40 +1,40 @@
-#largest_number=-99999999
-#counter =0
+largest_number=-99999999
+counter =0
 
-#while True:
- #   number=int(input("Enter a number or type -1  to end the program:"))
- #   if number ==-1:
-  #      break
-   # counter +=1
-    #if number > largest_number:
-    #    largest_number = number
-#if counter !=0:
- #   print("The largest number is ",largest_number)
-#else:
- #   print("You haven't entered any number")
+while True:
+   number=int(input("Enter a number or type -1  to end the program:"))
+   if number ==-1:
+      break
+   counter +=1
+  if number > largest_number:
+      largest_number = number
+ if counter !=0:
+    print("The largest number is ",largest_number)
+ else:
+    print("You haven't entered any number")
  
-#while True:
- #   word =input ("Enter a word:")
-  #  if word.lower() =="chupacabra":
-   #     print("you've successfully left the loop. ")
-    #    break
-#i=1
-#while i<5:
-  #  print(i)
- #   i+=1
-#else:
-    #print("else:",i)
-#for i in range(5):
- #   print(i)
-#else:
-   # print("else:",i)
+while True:
+   word =input ("Enter a word:")
+  if word.lower() =="chupacabra":
+     print("you've successfully left the loop. ")
+     break
+i=1
+while i<5:
+  print(i)
+   i+=1
+else:
+  print("else:",i)
+for i in range(5):
+    print(i)
+else:
+    print("else:",i)
    
-#i=111
-#for i in range(2,1):
- #   print(i)
-#else:
-    #print("else:",i)
-#def collatz(n):
+i=111
+for i in range(2,1):
+   print(i)
+else:
+   print("else:",i)
+def collatz(n):
     #count=0
     #while n !=1:
         #print(n)
